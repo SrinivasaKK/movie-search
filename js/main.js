ebe71093cf64c508fd284c35477a5560762a3039
srini_947 <srinivasa.keshavamurthy@chubb.com>
2021-08-14 22:36:55 +0800

    added new font and implemented toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const API_URL = 'http://www.omdbapi.com/?apikey=4d11b698&type=movie';
 const movieDOM = document.getElementById('main')
 const sessionStorageKey = 'movieID'
+const showDetailsButtonText = 'Show Details'
+const hideDetailsButtonText = 'Hide Details'
 function showMovies(url){
    let output = '';
     fetch(url).then(res => res.json())
@@ -9,14 +11,15 @@ function showMovies(url){
       if(response.Response=='True'){
       let movies = response.Search;
      
-      movies.forEach((movie) => {
+      movies.forEach((movie, index) => {
         output += `
           <div class='main'>
             <div class="card text-center" >
               <img src="${movie.Poster}" alt="poster">
               <div>
               <h4><b>${movie.Title}</b></h4>
-              <button  onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</button>
+              <button id = "button_${index}" onclick="movieSelected('${movie.imdbID}', '${index}')"  class="toggle-content">${showDetailsButtonText}</button>
+              <div class="content" id = "content_${index}" ></div>
               </div>
             </div>
           </div>
@@ -27,7 +30,6 @@ function showMovies(url){
     else {
       output = `<div>
             <div class="text-center">
-              
               <h5 class='error'>${response.Error} Please search again with specific movie name</h5>
             </div>
           </div>`
@@ -41,34 +43,45 @@ function showMovies(url){
     });
 }
 
-function movieSelected(id){
+function movieSelected(id, index){
+
+  // check if it is already cached 
  const movieDetails = sessionStorage.getItem(`${sessionStorageKey}_${id}`);
+ const content = document.getElementById(`content_${index}`);
+  const button  = document.getElementById(`button_${index}`);
+  
+   if (content.style.display === "block") {
+      content.style.display = "none";
+      button.innerHTML = showDetailsButtonText
+    } else {
+      content.style.display = "block";
+       button.innerHTML = hideDetailsButtonText
+    }
+   const coll = document.getElementsByClassName("toggle-content");
+   coll[index].classList.toggle('active')
+ 
+   if(movieDetails) {
+     content.innerHTML = movieDetails
+     return;
+   }
 
- if(movieDetails) {
-   // append details
-   console.log(movieDetails)
-   return;
- }
-
+   // if movie details are not already stored in session storage, fetch from api
   const url = `${API_URL}&i=${id}`
   fetch(url).then(res => res.json())
     .then((response)=>{
-    console.log(response)
-
     sessionStorage.setItem(`${sessionStorageKey}_${id}`,JSON.stringify(response));
-
+    content.innerHTML = JSON.stringify(response)
     })
  
 }
 
 
   function searchMovie() {
-    
     const form = document.getElementById("searchForm");
-    var elements = document.getElementById("searchForm").elements;
-   var obj ={};
-    for(var i = 0 ; i < elements.length ; i++){
-        var item = elements.item(i);
+    const elements = document.getElementById("searchForm").elements;
+    const obj ={};
+    for(let i = 0 ; i < elements.length ; i++){
+        let item = elements.item(i);
         obj[item.id] = item.value;
     }
     const searchTerm = obj.searchText;
@@ -83,16 +96,10 @@ function movieSelected(id){
 
 
 
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("searchdiv");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+window.onscroll = () =>{stickyHeader()};
+function stickyHeader() {
+const header = document.getElementById("searchdiv");
+const sticky = header.offsetTop;
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
   } else {
